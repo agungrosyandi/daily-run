1be27fda9beecf8c8ead554081676ac808c44827
narasihistorian <narasihistorian@gmail.com>
2024-07-27 13:10:08 +0700

    testing push

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export default function Title({ locked }) {
   return (
     <h1 className="text-4xl font-RobotoBold font-bold">{`${
-      locked ? 'Buy Premium' : 'Daily Run'
+      locked ? "Buy Premium" : "Daily Run testing"
     }`}</h1>
   );
 }
